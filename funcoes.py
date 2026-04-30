216567f0f99a7a05b2b3e5f2784e15aa93a42f98
Mariana <marianatg1@al.insper.edu.br>
2026-04-30 11:27:51 -0300

    segundo exercicio

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,20 @@ def rolar_dados (n):
         i = i +1
     return lista
 
+
+def guardar_dado (drol, dguar, n):
+    m = []
+    restantes = []
+    i = 0
+    while i<len(dguar):
+        m.append(dguar[i])
+        i = i +1
+    m.append(drol[n])
+    i = 0
+    while i<len(drol):
+        if i != n:
+            restantes.append(drol[i])
+        i = i +1
+
+    resultado = [restantes, m]
+    return resultado
